604c1f4201d99f6d3a843774c3515d6fcc8bc6d7
Chris Klemz <crklemz@gmail.com>
2021-05-17 20:17:38 -0500

    pics are clickable, working on click function

--- client.js
+++ client.js
@@ -1,11 +1,18 @@
 console.log('Here are all the available people:', people);
 
+let rName = '';
+
 $(document).ready(readyNow);
 
 function readyNow() {
     console.log('JQ');
     printPeople(people);
-    
+    randomName();
+
+    //click listeners
+
+    $('.pics').on('click', '.picture', handleClick);
+
 }
 
 function printPeople() {
@@ -14,14 +21,37 @@ function printPeople() {
     for(let info of people) {
 
     $('.pics').append(`
-    <div data-name=${info.name}"><img src="https://github.com/${info.githubUsername}.png?size=250" alt="alt="Profile image of ${info.name}"></div>
+    <div class="picture" data-name=${info.name}"><img src="https://github.com/${info.githubUsername}.png?size=250" alt="alt="Profile image of ${info.name}"></div>
     `)
-
-
-    
     }//end for   
 }//end printPeople
 
 function randomNumber(min, max){
     return Math.floor(Math.random() * (1 + max - min) + min);
 }
+
+function randomName() {
+  rName = $('.nameClick').text(`Click On: ${people[randomNumber(1, 10)].name}`);
+
+}
+
+function handleClick() {
+    let idOfClicked = $(this).data('name');
+    console.log(idOfClicked);
+    
+    //determine name of photo clicked
+    if(rName === idOfClicked) {
+
+    console.log('clickPic --> Success!');
+    $(this).text('Success!')
+
+    } else {
+        console.log('clickPic --> Wrong!');
+        $(this).text('Wrong!')
+    }
+    //test name clicked against the prompted name
+
+    
+    //run program again to generate another name prompt
+}
+
